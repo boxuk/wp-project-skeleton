908af900d6c85852268732a491705807584d8577
Ian Jenkins <ian.jenkins@boxuk.com>
2022-03-02 13:47:34 +0000

    [DEPS] Update wp-muplugin-loader to 2.0.
    https://github.com/boxuk/wp-muplugin-loader/releases/tag/2.0.0

--- wp-content/mu-plugins/zzz-mu-require.php
+++ wp-content/mu-plugins/zzz-mu-require.php
@@ -3,10 +3,10 @@
  * Plugin Name: MU Plugin Loader
  * Plugin URI: https://github.com/boxuk/wp-muplugin-loader
  * Description: MU Plugin Loader - Autoload your mu-plugin directories.
- * Version: 1.1.0
+ * Version: 2.0.0
  * Author: Box UK / Luke Woodward
  * Author URI: https://github.com/boxuk/wp-muplugin-loader
  *
  * @since 1.1.0
  */
-require_once __DIR__ . '/../vendor/boxuk/wp-muplugin-loader/src/lkwdwrd/mu-loader.php';
+require_once __DIR__ . '/../vendor/boxuk/wp-muplugin-loader/src/mu-loader.php';
